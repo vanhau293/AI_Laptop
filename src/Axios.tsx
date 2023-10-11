b45c1db1364096e1713a4f912b3cfdc72ed813f9
hauchauvan <hau.chauvan@unified.com>
2023-10-11 12:33:34 +0700

    intergate with backend

--- src/Axios.tsx
+++ src/Axios.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = 'https://ailaptopbe-production.up.railway.app';
+const BASE_URL = 'http://localhost:8080';
 
 export default axios.create({
     baseURL: BASE_URL,
