53843d9964fe5e03fc9ad1427aa1d0bc243a79a2
hauchauvan <hau.chauvan@unified.com>
2023-10-11 09:56:31 +0700

    update to deploy

--- src/Axios.tsx
+++ src/Axios.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://ailaptopbe-production.up.railway.app';
 
 export default axios.create({
     baseURL: BASE_URL,
